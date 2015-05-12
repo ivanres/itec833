042b3e163cb765a409c94e6d5dad29edf8d6fbc2
Ivan Rodriguez Asqui <iragination@gmail.com>
2015-05-12 16:52:43 +1000

    returns the inserted id

--- axis-logger-service/src/axis/logger/SqliteLoggerService.java
+++ axis-logger-service/src/axis/logger/SqliteLoggerService.java
@@ -1,23 +1,36 @@
 package axis.logger;
 import java.sql.*;
-//import org.sql.*;
 
+/**
+ * Service Entry Point
+ * @author Ivan Rodriguez Asqui
+ *
+ */
 public class SqliteLoggerService {
 
 	public int addLog(String cc_number, String cc_type){
 		try {
+			
+			//Inserts the log
 			Class.forName("org.sqlite.JDBC");
 			Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\databases\\logs.db");
 			String sql = "INSERT INTO logs (cc_number, cc_type) VALUES (?, ?)"; 
-			PreparedStatement stat = conn.prepareStatement(sql);
+			PreparedStatement stat = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 			stat.setString(1, cc_number);
 			stat.setString(2, cc_type);
-			stat.execute();
+			stat.executeUpdate();
+			
+			//Get the inserted id
+			ResultSet keys = stat.getGeneratedKeys();
+			if(keys.next()){
+				return keys.getInt(1);
+			}
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		return 79;
+		return -1;
 	}
 }
