d3002b8cc6bff310c6f69511071dcb0663a1a2ce
Ivan Rodriguez Asqui <iragination@gmail.com>
2015-05-19 22:17:44 +1000

    factored code into a class

--- frontend-python-bottle-suds/frontent-soap-tester.py
+++ frontend-python-bottle-suds/frontent-soap-tester.py
@@ -4,18 +4,28 @@
 
 from suds.client import Client
 import logging
-url = "http://localhost:8080/ode/processes/WS_Integrator?wsdl"
-location = "http://localhost:8080/ode/processes/WS_Integrator.WS_IntegratorPort" #Suds does not find the endpoint correctly, so we provide it manually
-client = Client(url, location=location)
-
-client.options.cache.clear()
-client.set_options(cache=None)
 
 
-logging.basicConfig(level=logging.INFO)
-logging.getLogger('suds.client').setLevel(logging.DEBUG)
 
-#Reset remote database
-result = client.service.process('4556394058714517')
-print("It is a %s credit card" % result)
+class BPEL:
+    url = "http://localhost:8080/ode/processes/WS_Integrator?wsdl"
+    location = "http://localhost:8080/ode/processes/WS_Integrator.WS_IntegratorPort" #Suds does not find the endpoint correctly, so we provide it manually
 
+    def __init__(self):
+        client = Client(self.url, location=self.location)
+
+        client.options.cache.clear()
+        client.set_options(cache=None)
+        self.client = client
+
+
+        logging.basicConfig(level=logging.INFO)
+        logging.getLogger('suds.client').setLevel(logging.DEBUG)
+
+    def process(self, cc_number):
+        return self.client.service.process(cc_number)
+
+if __name__ == '__main__':
+    srv = BPEL()
+    result = srv.process('4556394058714517')
+    print("It is a %s credit card" % result)
